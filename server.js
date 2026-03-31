0510cc388196b49567c054fba0ccab6566c2039f
Ansh Agarwal <anshagarwalcs1@gmail.com>
2026-03-31 20:17:59 +0530

    Refactor server.js to modern JavaScript syntax

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const fetch = (...args) => 
+const fetch = (...args) =>
+import("node-fetch").then(({ default: 
+fetch }) => fetch(...args));
 
 const PDFDocument = require("pdfkit");
 const multer = require("multer");
